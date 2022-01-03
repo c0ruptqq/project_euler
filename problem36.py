74cb288f7daf6d72b112553632f14184fc868c50
Maksym <max.d.astapov@gmail.com>
2022-01-03 11:39:21 +0000

    added problem 36

---
+++ problem36.py
@@ -0,0 +1,18 @@
+s = 1
+
+for i in range(1, 2047+1):
+    b = "{0:b}".format(i)
+    rb = b[::-1]
+    #print(b)
+    #print(rb)
+    for v in [b+rb, b+"0"+rb, b+"1"+rb]:
+        d = int(v, 2)
+        #print(d)
+        if d < 1000000 and str(d) == str(d)[::-1]:
+            print(v)
+            print(d)
+            s+= d
+
+print(s)
+
+
